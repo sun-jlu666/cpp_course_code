4779f9e2b47e3a12b265853f2d88c14977713e0c
sun-jlu666 <61871893+sun-jlu666@users.noreply.github.com>
2021-04-18 16:43:13 +0800

    2_1
    2_1

---
+++ 2_1/2_1/2_1.cpp
@@ -0,0 +1,23 @@
+// 2_1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int radius;
+	cout << "please enter the radius\n";
+	cin >> radius;
+	cout << "the radius is:" << radius << '\n';
+	cout << "PI is:" << 3.14 << '\n';
+	cout << "please enter a different radius\n";
+	cin >> radius;
+	cout << "now the radius is changed to:" << radius << '\n';
+
+
+    return 0;
+}
+
